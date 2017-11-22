e82365ef981ccb13f6beeecb0751266aac112a99
abdesslem <amriabdesslem@gmail.com>
2017-11-22 16:39:57 +0100

    create a pypi package

--- bmywp/main.py
+++ bmywp/main.py
@@ -9,20 +9,19 @@ API_SECRET = config.API_SECRET
 API_URL    = config.API_URL
 
 
-api_sign = hmac.new(API_SECRET, '/api/v1/strategies?id=1', hashlib.sha256).hexdigest()
-
+api_sign = hmac.new(API_SECRET, '/api/v1/strategies', hashlib.sha256).hexdigest()
 
 '''
 response = requests.get(
-  'https://test.bmybit.com/api/v1/strategies?id=1',
+  'https://test.bmybit.com/api/v1/strategies',
   headers={
     'X-Api-Key': API_KEY,
     'X-Api-Sign': api_sign
   },
 )
+
+print response.content
 '''
-
-
 
 
 bmybit = tortilla.wrap('https://test.bmybit.com/api/v1')
@@ -32,10 +31,10 @@ bmybit.config.headers['api_sign']  = api_sign
 
 print bmybit.config.headers
 
-try:
-    bmybit.get('strategies', params={'id': '1'})
-except:
-    print 'AUTH Error'
+res = bmybit.get('strategies',params = {'id':330},  headers={
+    'X-Api-Key': API_KEY,
+    'X-Api-Sign': api_sign
+  })
 
-
+print res
 
